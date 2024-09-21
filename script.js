4fcbf0fd0aeebd9b9c57cd045ebfd3655c778e33
AdrianHafiz <drianhafiz89@gmail.com>
2024-09-22 01:23:32 +0700

    Perubahan ui dan tambah fitur

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-document.getElementById('readMoreBtn1').addEventListener('click', function() {
+// Fitness
+document.getElementById('readMoreBtn1')?.addEventListener('click', function() {
     window.location.href = '5manolahraga.html';
 });
 
-document.getElementById('readMoreBtn2').addEventListener('click', function() {
-    window.location.href = 'resepatlit.html';
-});
-
-document.getElementById('readMoreBtn3').addEventListener('click', function() {
+document.getElementById('readMoreBtn3')?.addEventListener('click', function() {
     window.location.href = 'olahmental.html';
 });
 
-document.getElementById('readMoreBtn4').addEventListener('click', function() {
-    window.location.href = 'diet.html';
+document.getElementById('readMoreBtn99')?.addEventListener('click', function() {
+    window.location.href = 'jenisoalhraga.html';
 });
 
-document.getElementById('readMoreBtn5').addEventListener('click', function() {
-    window.location.href = 'sehatseharihari.html';
+// Resep
+document.getElementById('readMoreBtn2')?.addEventListener('click', function() {
+    window.location.href = 'resepatlit.html';
 });
 
-document.getElementById('readMoreBtn99').addEventListener('click', function() {
-    window.location.href = 'jenisolahraga.html';
+document.getElementById('readMoreBtn4')?.addEventListener('click', function() {
+    window.location.href = 'diet.html'; 
+});
+
+document.getElementById('readMoreBtn5')?.addEventListener('click', function() {
+    window.location.href = 'ssehatseharihari.html'; 
 });
