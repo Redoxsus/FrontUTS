f9c59b114d9c575a46af3a2fc8c1305a824cb381
DarahBeku <bryan.535230012@stu.untar.ac.id>
2024-09-21 16:50:54 +0700

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('readMoreBtn1').addEventListener('click', function() {
+    window.location.href = '5manolahraga.html';
+});
+
+document.getElementById('readMoreBtn2').addEventListener('click', function() {
+    window.location.href = 'resepatlit.html';
+});
+
+document.getElementById('readMoreBtn3').addEventListener('click', function() {
+    window.location.href = 'olahmental.html';
+});
+
+document.getElementById('readMoreBtn4').addEventListener('click', function() {
+    window.location.href = 'diet.html';
+});
+
+document.getElementById('readMoreBtn5').addEventListener('click', function() {
+    window.location.href = 'sehatseharihari.html';
+});
+
+document.getElementById('readMoreBtn99').addEventListener('click', function() {
+    window.location.href = 'jenisolahraga.html';
+});
---
+++ script2.js
@@ -0,0 +1,11 @@
+document.getElementById('readMoreBtn2').addEventListener('click', function() {
+    window.location.href = 'resepatlit.html';
+});
+
+document.getElementById('readMoreBtn4').addEventListener('click', function() {
+    window.location.href = 'diet.html';
+});
+
+document.getElementById('readMoreBtn5').addEventListener('click', function() {
+    window.location.href = 'sehatseharihari.html';
+});
