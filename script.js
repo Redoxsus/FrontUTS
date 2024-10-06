b5f8b706fd2678f27aa81bf427faa659b97b0e35
Bryan Fidello <bryfid857@gmail.com>
2024-10-06 13:12:24 +0700

    redesign

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ document.getElementById('readMoreBtn4')?.addEventListener('click', function() {
 document.getElementById('readMoreBtn5')?.addEventListener('click', function() {
     window.location.href = 'sehatseharihari.html'; 
 });
+
+// Select the navbar
+const navbar = document.getElementById('navbar');
+
+// Add a scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) { // Adjust scroll distance as needed
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+document.querySelector('.cta-button').addEventListener('click', function (e) {
+    e.preventDefault(); 
+    document.querySelector('#content').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
